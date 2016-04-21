a4bcaa391e85d6673facc59e6df94b949df91db2
aignat <alexandru.ignat@yahoo.com>
2016-04-21 18:00:36 +0300

    Initial commit

---
+++ src/main/java/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package main.java.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by aignat on 21-Apr-16.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
